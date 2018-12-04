275e31691d70eb1d22d8149b813c1178c0110497
chinasixth <1961631077@qq.com>
2018-12-04 10:12:47 +0800

    sixth commit

---
+++ src/main/java/com/sixth/sessionanalyze1707/dao/IAdStatDAO.java
@@ -0,0 +1,17 @@
+package com.sixth.sessionanalyze1707.dao;
+
+
+import com.sixth.sessionanalyze1707.domain.AdStat;
+
+import java.util.List;
+
+/**
+ * 广告实时统计DAO接口
+ * @author Administrator
+ *
+ */
+public interface IAdStatDAO {
+
+	void updateBatch(List<AdStat> adStats);
+	
+}
---
+++ src/main/java/com/sixth/sessionanalyze1707/dao/impl/Top10CategoryDAOImpl.java
@@ -0,0 +1,28 @@
+package com.sixth.sessionanalyze1707.dao.impl;
+
+import com.sixth.sessionanalyze1707.dao.ITop10CategoryDAO;
+import com.sixth.sessionanalyze1707.domain.Top10Category;
+import com.sixth.sessionanalyze1707.jdbc.JDBCHelper;
+
+/**
+ * top10品类DAO实现
+ * @author Administrator
+ *
+ */
+public class Top10CategoryDAOImpl implements ITop10CategoryDAO {
+
+	@Override
+	public void insert(Top10Category category) {
+		String sql = "insert into top10_category values(?,?,?,?,?)";  
+		
+		Object[] params = new Object[]{category.getTaskid(),
+				category.getCategoryid(),
+				category.getClickCount(),
+				category.getOrderCount(),
+				category.getPayCount()};  
+		
+		JDBCHelper jdbcHelper = JDBCHelper.getInstance();
+		jdbcHelper.executeUpdate(sql, params);
+	}
+
+}
